11cec6258ebe264a933749897afdecf63354ed2c
Kalio007 <Kalioprince1@gmail.com>
2022-12-03 12:14:34 +0100

    gitignoring

--- src/components/lastest.js
+++ src/components/lastest.js
@@ -4,9 +4,12 @@ import React from "react";
 
 export default function TheLastest(){
     return (
+        <div>
         <h1 className="latest">
             Latest News...
         </h1>
+        <img href="./components/air.svg" alt=""/>
+        </div>
 
     )
 }
